0ba004050919851cdef7af8fc2f6e650a62dc893
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-12-12 22:57:27 -0800

    Add hit method

--- src/functions.js
+++ src/functions.js
@@ -1,21 +1,32 @@
+/* eslint-disable import/prefer-default-export */
 class Ship {
   constructor(type) {
     this.type = type;
-    this.hit = 0;
     this.sunk = false;
 
-    if (type == 'carrier') {
+    if (type === 'carrier') {
       this.length = 5;
-    } else if (type == 'battleship') {
+      this.hitPoints = 5;
+    } else if (type === 'battleship') {
       this.length = 4;
-    } else if (type == 'cruiser' || type == 'submarine') {
+      this.hitPoints = 4;
+    } else if (type === 'cruiser' || type === 'submarine') {
       this.length = 3;
+      this.hitPoints = 3;
     } else {
       this.length = 2;
+      this.hitPoints = 2;
+    }
+  }
+
+  hit() {
+    this.hitPoints -= 1;
+    if (this.hitPoints === 0) {
+      this.sunk = true;
     }
   }
 }
 
 export {
   Ship,
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-unused-vars */
 import './style.css';
+import { Ship } from './functions';
 
-console.log('Webpack test');
+const carrier = new Ship('carrier');
+const battleship = new Ship('battleship');
+const cruiser = new Ship('cruiser');
+const submarine = new Ship('submarine');
+const destroyer = new Ship('destroyer');
