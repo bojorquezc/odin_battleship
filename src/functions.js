9641c6e49cfc3289d6f05f5c3dee3f4bb5eaabf3
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-12-12 22:29:51 -0800

    Start Ship class

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,21 @@
-function sum(a, b) {
-    return a + b;
-  }
+class Ship {
+  constructor(type) {
+    this.type = type;
+    this.hit = 0;
+    this.sunk = false;
 
-  export {
-    sum,
+    if (type == 'carrier') {
+      this.length = 5;
+    } else if (type == 'battleship') {
+      this.length = 4;
+    } else if (type == 'cruiser' || type == 'submarine') {
+      this.length = 3;
+    } else {
+      this.length = 2;
+    }
   }
+}
+
+export {
+  Ship,
+}
